3c35a66f7283cc10e2443779ad1fc5cef2c552c5
sunakan <sunakan03@gmail.com>
2019-09-15 14:30:25 +0900

    [Add] Shiftonlyの別解

--- atcoder/ABC081B-Shiftonly/src/main.rs
+++ atcoder/ABC081B-Shiftonly/src/main.rs
@@ -4,7 +4,9 @@ fn main() {
     std::io::stdin().read_line(&mut s);
     // 本番
     let arr = read_vec();
-    let ans = solv(arr, 0);
+    // 再帰用
+    //let ans = solv(arr, 0);
+    let ans = solv(arr);
     println!("{}", ans);
 }
 
@@ -16,14 +18,23 @@ fn read_vec<T: std::str::FromStr>() -> Vec<T> {
 }
 
 // 再帰
-fn solv(arr: Vec<u32>, count: u32) -> u32 {
-    let even_only_arr: Vec<u32> = arr.clone().into_iter().filter(|x| x%2 == 0).collect();
-    if arr.iter().count() == even_only_arr.iter().count() {
-        let next: Vec<u32> = even_only_arr.into_iter().map(|x| x / 2).collect();
-        solv(next, count + 1)
-    } else {
-        count
-    }
+//fn solv(arr: Vec<u32>, count: u32) -> u32 {
+//    let even_only_arr: Vec<u32> = arr.clone().into_iter().filter(|x| x%2 == 0).collect();
+//    if arr.iter().count() == even_only_arr.iter().count() {
+//        let next: Vec<u32> = even_only_arr.into_iter().map(|x| x / 2).collect();
+//        solv(next, count + 1)
+//    } else {
+//        count
+//    }
+//}
+
+// 別解(こっちのほうがスマート)
+// 2進数にしたとき1の位から何回1が連続しているか
+// minはOptionを返すが、今回は1以上であることを問題が保証しているため、unwrapしてOKとのこと
+// メソッドチェインしてもよかったが、今回は上の解説コメントを入れるために分けた
+fn solv(arr: Vec<u32>) -> u32 {
+    let ans = arr.into_iter().map(|v| v.trailing_zeros()).min();
+    ans.unwrap()
 }
 
 #[cfg(test)]
@@ -31,15 +42,21 @@ mod tests {
     use super::*;
     #[test]
     fn test1() {
-        assert_eq!(2, solv(vec! [8, 12, 40], 0));
+        //再帰用
+        //assert_eq!(2, solv(vec! [8, 12, 40], 0));
+        assert_eq!(2, solv(vec! [8, 12, 40]));
     }
     #[test]
     fn test2() {
-        assert_eq!(0, solv(vec! [5, 6, 8, 10], 0));
+        //再帰用
+        //assert_eq!(0, solv(vec! [5, 6, 8, 10], 0));
+        assert_eq!(0, solv(vec! [5, 6, 8, 10]));
     }
     #[test]
     fn test3() {
         let arr = vec! [382253568, 723152896, 37802240, 379425024, 404894720, 471526144];
-        assert_eq!(8, solv(arr, 0));
+        //再帰用
+        //assert_eq!(8, solv(arr, 0));
+        assert_eq!(8, solv(arr));
     }
 }
