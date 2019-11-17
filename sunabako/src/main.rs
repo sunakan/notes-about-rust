282a18ec043ecb184a44dd17e38074b34423ddbc
sunakan <sunakan03@gmail.com>
2019-11-17 12:06:44 +0900

    [Add] assertの練習

--- sunabako/src/main.rs
+++ sunabako/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", hello());
+}
+
+fn hello() -> String {
+    return "hello".to_string();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    // assertの練習
+    #[test]
+    fn test_assert() {
+        assert_eq!(true, true);
+        assert_ne!(true, false);
+        assert_eq!("true", "true");
+        assert_ne!("true", "false");
+        //assert!(false, "always false"); // 必ず失敗する
+        assert!(true, "always true"); // 必ず成功する
+        let a = 3;
+        let b = 3;
+        assert!(a == b, "required {} == {}", a, b);
+    }
 }
